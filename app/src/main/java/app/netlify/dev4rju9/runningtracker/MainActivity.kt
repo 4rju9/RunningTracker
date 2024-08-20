b7df9b7b36bb4a187f932d0fc0320b17c673daba
4rju9 <ohinarendra3550@gmail.com>
2024-08-20 22:38:42 +0530

    Initial Commit: Added Gradle Dependencies and layout files

---
+++ app/src/main/java/app/netlify/dev4rju9/runningtracker/MainActivity.kt
@@ -0,0 +1,18 @@
+package app.netlify.dev4rju9.runningtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}

60807806f7354f9a36c07f0c166083cd802e0aff
4rju9 <ohinarendra3550@gmail.com>
2024-08-22 23:14:46 +0530

    Set-up the Map's Lifecycle

--- app/src/main/java/app/netlify/dev4rju9/runningtracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/app/netlify/dev4rju9/runningtracker/ui/fragments/TrackingFragment.kt
@@ -1,14 +1,62 @@
 package app.netlify.dev4rju9.runningtracker.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import app.netlify.dev4rju9.runningtracker.R
+import app.netlify.dev4rju9.runningtracker.databinding.FragmentTrackingBinding
 import app.netlify.dev4rju9.runningtracker.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
     private val viewModel: MainViewModel by viewModels()
+    private var map: GoogleMap? = null
+    private lateinit var binding: FragmentTrackingBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentTrackingBinding.bind(view)
+
+        binding.mapView.onCreate(savedInstanceState)
+        binding.mapView.getMapAsync {
+            map = it
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView.onLowMemory() 
+    }
 
 }

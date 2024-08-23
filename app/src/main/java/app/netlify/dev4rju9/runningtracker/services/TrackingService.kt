dfcbfd1031ef0e0a12ea87b52ee07fb40f654267
4rju9 <ohinarendra3550@gmail.com>
2024-08-23 19:49:57 +0530

    Changed TrackingService into a ForegroundService

--- app/src/main/java/app/netlify/dev4rju9/runningtracker/services/TrackingService.kt
+++ app/src/main/java/app/netlify/dev4rju9/runningtracker/services/TrackingService.kt
@@ -1,20 +1,42 @@
 package app.netlify.dev4rju9.runningtracker.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import app.netlify.dev4rju9.runningtracker.R
 import app.netlify.dev4rju9.runningtracker.other.Constants.ACTION_PAUSE_SERVICE
+import app.netlify.dev4rju9.runningtracker.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import app.netlify.dev4rju9.runningtracker.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import app.netlify.dev4rju9.runningtracker.other.Constants.ACTION_STOP_SERVICE
+import app.netlify.dev4rju9.runningtracker.other.Constants.NOTIFICATION_CHANNEL_ID
+import app.netlify.dev4rju9.runningtracker.other.Constants.NOTIFICATION_CHANNEL_NAME
+import app.netlify.dev4rju9.runningtracker.other.Constants.NOTIFICATION_ID
+import app.netlify.dev4rju9.runningtracker.ui.MainActivity
 import timber.log.Timber
 
 class TrackingService : LifecycleService() {
+
+    private var isFirstRun = true
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
         intent?.let {
             when (intent.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    Timber.d("Started or resumed service")
+                    if (isFirstRun) {
+                        startForegroundService()
+                        isFirstRun = false
+                    } else {
+                        Timber.d("Resuming service")
+                    }
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused Service")
@@ -26,6 +48,41 @@ class TrackingService : LifecycleService() {
         }
 
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun getMainActivityPendingIntent () = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        FLAG_UPDATE_CURRENT
+    )
+
+    private fun startForegroundService () {
+        val notificatonManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+            createNotificationChannel(notificatonManager)
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running Tracker")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel (notificatoinManager: NotificationManager) {
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            IMPORTANCE_LOW
+        )
+        notificatoinManager.createNotificationChannel(channel)
     }
 
 }

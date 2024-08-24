06e49d7e5453299cb5c14ea85af8fc660d3e0190
4rju9 <ohinarendra3550@gmail.com>
2024-08-24 19:31:03 +0530

    Implemented the functionality to sort the runs

--- app/src/main/java/app/netlify/dev4rju9/runningtracker/ui/fragments/RunFragment.kt
+++ app/src/main/java/app/netlify/dev4rju9/runningtracker/ui/fragments/RunFragment.kt
@@ -4,6 +4,7 @@ import android.Manifest
 import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import app.netlify.dev4rju9.runningtracker.R
 import app.netlify.dev4rju9.runningtracker.databinding.FragmentRunBinding
 import app.netlify.dev4rju9.runningtracker.other.Constants.REQUEST_CODE_LOCATION_PERMISSION
+import app.netlify.dev4rju9.runningtracker.other.SortType
 import app.netlify.dev4rju9.runningtracker.other.TrackingUtility
 import app.netlify.dev4rju9.runningtracker.ui.fragments.adapters.RunAdapter
 import app.netlify.dev4rju9.runningtracker.ui.viewmodels.MainViewModel
@@ -35,7 +37,29 @@ class RunFragment : Fragment(R.layout.fragment_run), EasyPermissions.PermissionC
         requestPermissions()
         setupRecyclerView()
 
-        viewModel.runsSortedByDate.observe(viewLifecycleOwner, Observer {
+        when (viewModel.sortType) {
+            SortType.DATE -> binding.spFilter.setSelection(0)
+            SortType.RUNNING_TIME -> binding.spFilter.setSelection(1)
+            SortType.DISTANCE -> binding.spFilter.setSelection(2)
+            SortType.AVG_SPEED -> binding.spFilter.setSelection(3)
+            SortType.CALORIES_BURNED -> binding.spFilter.setSelection(4)
+        }
+
+        binding.spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                when (position) {
+                    0 -> viewModel.sortRuns(SortType.DATE)
+                    1 -> viewModel.sortRuns(SortType.RUNNING_TIME)
+                    2 -> viewModel.sortRuns(SortType.DISTANCE)
+                    3 -> viewModel.sortRuns(SortType.AVG_SPEED)
+                    4 -> viewModel.sortRuns(SortType.CALORIES_BURNED)
+                }
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
+
+        viewModel.runs.observe(viewLifecycleOwner, Observer {
             runAdapter.submitList(it)
         })
 
